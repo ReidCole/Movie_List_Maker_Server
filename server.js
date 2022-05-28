9201e7b7116dc23acc764b4a0c1c2aa1f94b6181
Kininari <reid-cole@outlook.com>
2022-05-28 14:42:06 -0400

    changed console log

--- server.js
+++ server.js
@@ -306,7 +306,7 @@ function authenticateToken(req, res, next) {
   const authHeader = req.headers["authorization"];
   const token = authHeader ? authHeader.split(" ")[1] : null;
   if (token === null) {
-    console.log("token is null");
+    console.log("access token is not provided in header");
     return res.sendStatus(401);
   }
   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, userObj) => {
